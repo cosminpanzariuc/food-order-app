4e1bb953bbe30fced129262d846352322794ad65
Cosmin Panzariuc <cosminilie.panzariuc@cognizant.com>
2025-09-24 09:49:18 +0300

    updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import {currencyFormatter} from "../util/formatting.js";
 import Button from "../UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
@@ -21,9 +22,13 @@ export default function Cart() {
     return <Modal className='cart' open={userProgressCtx.progress === 'cart'}>
         <h2>Your Cart</h2>
         <ul>
-            {cartCtx.items.map((item) => <li key={item.id}>
-                {item.name} - {item.quantity}
-            </li>)}
+            {cartCtx.items.map((item) =>
+                (<CartItem
+                        key={item.id}
+                        {...item}
+                        onIncrease={() => cartCtx.addItem(item)}
+                        onDecrease={() => cartCtx.removeItem(item.id)}/>
+                ))}
         </ul>
         <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
         <p className='modal-actions'>
