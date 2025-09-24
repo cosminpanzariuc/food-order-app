e2f3408968b7aa1ec98d35fd8f48e050bb6a84db
Cosmin Panzariuc <cosminilie.panzariuc@cognizant.com>
2025-09-24 12:30:13 +0300

    fix bug

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,7 @@
 // import {useEffect, useState} from "react";
 import MealItem from "./MealItem.jsx";
 import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -33,7 +34,7 @@ export default function Meals() {
         return <Error title='Failed to fetch meals' message={error}/>
     }
 
-    return <ul id='meals'>{loadedMeals.map((meal) =>
+    return (<ul id='meals'>{loadedMeals.map((meal) =>
         <MealItem key={meal.id} meal={meal}/>)}
-    </ul>
+    </ul>)
 }
